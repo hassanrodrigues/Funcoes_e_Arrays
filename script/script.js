cf0c7de9cbd27d1d8030c3c058ea4576cc738600
hassanrodrigues <hassanrodrigues14@gmail.com>
2022-04-12 13:38:01 -0400

    Atualizando codigo com pequenos ajustes na funcao

--- script/script.js
+++ script/script.js
@@ -1,40 +1,53 @@
 function positions(fisrtPlace, secondPlace, lastPlace) {
     let arrayAtletas = [{
-            name: fisrtPlace,
-            tarefaExtra: true
-        },
-        {
-            name: secondPlace,
+            nome: fisrtPlace,
             tarefaExtra: false
         },
         {
-            name: lastPlace,
+            nome: secondPlace,
+            tarefaExtra: false
+        },
+        {
+            nome: lastPlace,
             tarefaExtra: true
         }
     ]
-    console.log(arrayAtletas)
-
+    //console.log(arrayAtletas)
+    console.log(
+        `---Chegada---
+    1ª - Colocado ${arrayAtletas[0].nome}
+    2ª - Colocado ${arrayAtletas[1].nome} 
+    3ª - Colocado ${arrayAtletas[2].nome}  
+        `
+    )
+    console.log("Tarefas Extras")
     if (arrayAtletas[0].tarefaExtra == true) {
-        console.log("Completou a tarefa extra e subiu uma posicao");
-        mudarPosicaoAtleta((arrayAtletas.length - 1), arrayAtletas, 1)
+        console.log(`${arrayAtletas[0].nome} completou a tarefa extra e subiu uma posicao`)
+        mudarPosicaoAtleta(0, arrayAtletas, 0)
     }
     if (arrayAtletas[1].tarefaExtra == true) {
-        console.log("Completou a tarefa extra e subiu uma posicao")
-        mudarPosicaoAtleta((arrayAtletas.length - 1), arrayAtletas, 1)
+        console.log(`${arrayAtletas[1].nome} completou a tarefa extra e subiu uma posicao`)
+        mudarPosicaoAtleta(1, arrayAtletas, 0)
     }
     if (arrayAtletas[2].tarefaExtra == true) {
-        console.log("Completou a tarefa extra e subiu uma posicao")
-        mudarPosicaoAtleta((arrayAtletas.length - 1), arrayAtletas, 1)
+        console.log(`${arrayAtletas[2].nome} completou a tarefa extra e subiu uma posicao`)
+        mudarPosicaoAtleta(2, arrayAtletas, 1)
+
     }
-    console.log(arrayAtletas)
+    //console.log(arrayAtletas)
+
+    return `
+    Resultado final
+    1ª - Colocado ${arrayAtletas[0].nome}
+    2ª - Colocado ${arrayAtletas[1].nome} 
+    3ª - Colocado ${arrayAtletas[2].nome}  
+    `
 
 }
 
-function mudarPosicaoAtleta(posicaoInicio, arrayAtletas, posicaoFinal) {
-    arrayAtletas.splice(posicaoFinal, 0, arrayAtletas.splice(posicaoInicio, 1)[0]);
+function mudarPosicaoAtleta(posicaoChegada, arrayAtletas, posicaoFinalExtra) {
+    arrayAtletas.splice(posicaoFinalExtra, 0, arrayAtletas.splice(posicaoChegada, 1)[0]);
     return arrayAtletas;
 }
 
-
-
-positions('rafael', 'hassan', 'daniel')
+console.log(positions('Rafael', 'Manoel', 'Daniel'))
