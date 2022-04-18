618c884f14031579b3557830347a9d8f6318d0dc
hassanrodrigues <hassanrodrigues14@gmail.com>
2022-04-18 13:17:15 -0400

    tirando linhas

--- script/script.js
+++ script/script.js
@@ -1,24 +1,21 @@
 function positions(fisrtPlace, secondPlace, lastPlace) {
     let atletas = [fisrtPlace, secondPlace, lastPlace]
-  //segundo pra primeira
-    if (secondPlace===`Daniel`) {
-        atletas[0]=atletas[1]
-        atletas[1]=fisrtPlace
+    //segundo pra primeira
+    if (secondPlace === `Daniel`) {
+        atletas[0] = atletas[1]
+        atletas[1] = fisrtPlace
     }
-
-    if(lastPlace===`Daniel`) {
-        atletas[1]=atletas[2]
-        atletas[2]=secondPlace   
-    }  
+    if (lastPlace === `Daniel`) {
+        atletas[1] = atletas[2]
+        atletas[2] = secondPlace
+    }
     //console.log(arrayAtletas)
     return `
     Resultado final
     1ª - Colocado ${atletas[0]}
     2ª - Colocado ${atletas[1]} 
     3ª - Colocado ${atletas[2]} `
-
 }
-
 console.log(`Teste 1`)
 console.log(positions('Rafael', 'Manoel', 'Daniel'))
 console.log(`Teste 2`)
